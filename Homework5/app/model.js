7ea4a6bf4444534fcb87c48f8b90a7351401f0a3
jujoiner <54684212+jujoiner@users.noreply.github.com>
2022-10-10 01:29:46 -0400

    Add files via upload

---
+++ Homework5/app/model.js
@@ -0,0 +1,367 @@
+var homeContent = `<div class="blogs">
+<div class="blog">
+    <div class="blog-content">
+        <div class="blog-image">
+            <div class="offer">
+                <div class="blog-text">
+                    <div class="offer-text">
+                        <p>A PLACE IS IS NOT A PLACE</p>
+                    </div>
+                    <div class="offer-country">
+                        <span>UNTILL IT HAS A BOOKSTORE</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+<section class="promo-tours">
+<div class="promo-books">
+    <div class="promo tour1">
+    </div>
+    <p class="country">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperi explicabo.</p>
+    <h3 class="price">$15.99</h3>
+    <button>ADD TO CART</button>
+</div>
+<div class="promo-books">
+    <div class="promo tour2">
+    </div>
+    <p class="country">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam explicabo.</p>
+    <h3 class="price">$15.99</h3>
+    <button>ADD TO CART</button>
+</div>
+<div class="promo-books">
+    <div class="promo tour3">
+    </div>
+    <p class="country">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam remicabo.</p>
+    <h3 class="price">$15.99</h3>
+    <button>ADD TO CART</button>
+</div>
+</section>
+`;
+
+var aboutContent = `<div class="about">
+<section class="clients">
+    <p class="about-heading">ABOUT THE BOOKNOOK</p>
+    <hr>
+    <div class="client">
+        <div class="image client-1">
+        </div>
+        <div class="client-text">
+            <p class="about-heading">Founded in 2008</p>
+            <p class="quote">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, s unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+    </div>
+    
+    <div class="client">
+        <div class="image client-2">
+        </div>
+        <div class="client-text">
+            <p class="about-heading">FIND THE PERFECT BOOK</p>
+            <p class="quote">Ss iste natus error sit voluptatem accusantium doloremque laudantium, s unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, toerspiciatis u omnis iste natus errornde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem Seicabo. </p>
+        </div>
+    </div>
+</section>
+</div>`;
+
+var booksContent = `<div class="tours-hero">
+<p>TOURS:</p>
+</div>
+<div class="special-heading">
+<p><span>Black History Books</span></p>
+</div>
+<div class="tours">
+<div class="tours">
+    <div class="tour-image tour1"></div>
+    <div class="offer-content">
+        <div class="offer-text">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+        <div class="offer-country">
+            <span>$99.99</span>
+        </div>
+        <div class="offer-button">
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+</div>
+<div class="tours">
+    <div class="tour-image tour2"></div>
+    <div class="offer-content">
+        <div class="offer-text">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+        <div class="offer-country">
+            <span>$99.99</span>
+        </div>
+        <div class="offer-button">
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+</div>
+<div class="tours">
+    <div class="tour-image tour3"></div>
+    <div class="offer-content">
+        <div class="offer-text">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+        <div class="offer-country">
+            <span>$99.99</span>
+        </div>
+        <div class="offer-button">
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+</div>
+</div>
+<div class="special-heading">
+<p><span>Horror Books</span></p>
+</div>
+
+<div class="tours">
+    <div class="tour-image tour4"></div>
+    <div class="offer-content">
+        <div class="offer-text">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+        <div class="offer-country">
+            <span>$99.99</span>
+        </div>
+        <div class="offer-button">
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+</div>
+<div class="tours">
+    <div class="tour-image tour5"></div>
+    <div class="offer-content">
+        <div class="offer-text">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+        <div class="offer-country">
+            <span>$99.99</span>
+        </div>
+        <div class="offer-button">
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+</div>
+<div class="tours">
+    <div class="tour-image tour6"></div>
+    <div class="offer-content">
+        <div class="offer-text">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+        <div class="offer-country">
+            <span>$99.99</span>
+        </div>
+        <div class="offer-button">
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+</div>
+</div>
+<div class="special-heading">
+<p><span>Childrens Books</span></p>
+</div>
+<div class="tours">
+    <div class="tour-image tour7"></div>
+    <div class="offer-content">
+        <div class="offer-text">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+        <div class="offer-country">
+            <span>$99.99</span>
+        </div>
+        <div class="offer-button">
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+</div>
+<div class="tours">
+    <div class="tour-image tour8"></div>
+    <div class="offer-content">
+        <div class="offer-text">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+        <div class="offer-country">
+            <span>$99.99</span>
+        </div>
+        <div class="offer-button">
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+</div>
+<div class="tours">
+    <div class="tour-image tour9"></div>
+    <div class="offer-content">
+        <div class="offer-text">
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+        <div class="offer-country">
+            <span>$99.99</span>
+        </div>
+        <div class="offer-button">
+            <button>ADD TO CART</button>
+        </div>
+    </div>
+</div>
+</div>`;
+
+var blogContent = `<div class="blogs">
+<div class="blog-title">
+    <div>BLOG:</div>
+</div>
+<div class="blog">
+    <div class="blog-content">
+        <div class="blog-image">
+            <div class="offer">
+    
+                <div class="blog-text">
+                    <div class="percentage">(20% OFF!)</div>
+                    <div class="offer-text">
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                    </div>
+                    <div class="offer-country">
+                        <span>$99.99</span>
+                    </div>
+                    <div class="offer-button">
+                        <button>ADD TO CART</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="blog">
+    <div class="blog-content">
+        <div class="blog-image">
+            <div class="offer">
+    
+                <div class="blog-text">
+                    <div class="percentage">(20% OFF!)</div>
+                    <div class="offer-text">
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                    </div>
+                    <div class="offer-country">
+                        <span>$99.99</span>
+                    </div>
+                    <div class="offer-button">
+                        <button>ADD TO CART</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="blog">
+    <div class="blog-content">
+        <div class="blog-image">
+            <div class="offer">
+    
+                <div class="blog-text">
+                    <div class="percentage">(20% OFF!)</div>
+                    <div class="offer-text">
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                    </div>
+                    <div class="offer-country">
+                        <span>$99.99</span>
+                    </div>
+                    <div class="offer-button">
+                        <button>ADD TO CART</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`
+
+var accountContent = `<div class="contact">
+<div class="contact-heading">
+    <div>CONTACT US:</div>
+</div>
+<div class="contact-content">
+    <div class="contact-right">
+        <div class="contact-heading">
+            <div>Login Here!</div>
+        </div>
+        <div class="contact-form">
+            <form>
+                <input type="text" placeholder="Email Address:">
+                <input type="text" placeholder="Password:">
+            </form>
+        </div>
+        <div class="contact-button">
+            <button>LOGIN</button>
+        </div>
+        
+    </div>
+    <div class="contact-right">
+        <div class="contact-heading">
+            <div>Sign Up!</div>
+        </div>
+        <div class="contact-form">
+            <form>
+                <input type="text" placeholder="First Name:">
+                <input type="text" placeholder="Last Name:">
+                <input type="text" placeholder="Email Address:">
+                <input type="text" placeholder="Password:">
+            </form>
+        </div>
+        <div class="contact-button">
+            <button>SIGN UP</button>
+        </div>
+        
+    </div>
+</div>
+</div>`
+
+var cartContent = `<div class="special-offers">
+<div class="special-heading">
+    <p><span>Special Offers:</span></p>
+</div>
+<div class="offer">
+    <div class="offer-image offer1"></div>
+    <div class="offer-content">
+        <div class="offer-country">
+            <span>Bangkok, Thailand</span>
+        </div>
+        <div class="percentage">(20% OFF!)</div>
+        
+        <div class="offer-country">
+            <span>$99.99</span>
+            <div>In Stock</div>
+        </div>
+        <div>Qty:1 change | delete</div>
+    </div>
+</div>
+<div class="offer">
+    <div class="offer-image offer2"></div>
+    <div class="offer-content">
+        <div class="offer-country">
+            <span>Bangkok, Thailand</span>
+        </div>
+        <div class="percentage">(10% OFF!)</div>
+        
+        <div class="offer-country">
+            <span>$99.99</span>
+            <div>In Stock</div>
+        </div>
+        <div>Qty:1 change | delete</div>
+    </div>
+</div>
+</div>`
+
+function setCurrentPageContent(pageID) {
+  let contentName = pageID + "Content";
+  $("#app").html(eval(contentName));
+}
+
+function getLastName(lastName) {
+  console.log();
+}
+
+export { setCurrentPageContent };
